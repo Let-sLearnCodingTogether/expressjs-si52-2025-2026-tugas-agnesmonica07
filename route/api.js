3762e8716cef794506eaed454d4294703246c5e6
agnesmonica07 <agnesmonica071205@gmail.com>
2025-10-25 14:49:31 +0700

    update api routes for auth endpoints

--- route/api.js
+++ route/api.js
@@ -1,11 +1,17 @@
 import express from "express";
+import * as authController from "../controllers/authController.js";
 import * as garansiController from "../controllers/garansiController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const api = express.Router();
 
-api.get("/garansi", garansiController.listGaransi);
-api.post("/garansi", garansiController.createNewGaransi);
-api.put("/garansi/:id", garansiController.updateGaransi);
-api.delete("/garansi/:id", garansiController.deleteGaransi);
+api.get("/garansi", protect, garansiController.listGaransi);
+api.post("/garansi", protect, garansiController.createNewGaransi);
+api.put("/garansi/:id", protect, garansiController.updateGaransi);
+api.delete("/garansi/:id", protect, garansiController.deleteGaransi);
+
+api.post('/register', authController.register);
+api.post('/login', authController.login);
+api.get('/profile', protect, authController.getProfile);
 
 export default api;
